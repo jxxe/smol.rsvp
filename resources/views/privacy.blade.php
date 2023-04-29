c6af70bc9680f42c7a20a9709fe0816ac1ad2050
Jerome Paulos <jeromepaulos@yahoo.com>
2023-04-29 12:14:11 -0500

    Attempt to comply with Google's guidelines

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -2,15 +2,21 @@
     <div>
         <h1 class="text-2xl font-bold mb-2">Privacy Policy</h1>
     
-        <h2 class="text-xl font-bold mb-2">Data smol.rsvp Collects</h2>
+        <h2 class="text-xl font-bold mb-2">What data we store</h2>
         <ul class="list-disc list-inside marker:text-blue-500">
             <li>The name and email associated with your Google account</li>
             <li>Your Google refresh token so we can securely access your account</li>
             <li>The IDs of any Google Calendar events you attach to an invitation</li>
         </ul>
     
-        <h2 class="text-xl font-bold mt-6 mb-2">Deleting Your Data</h2>
+        <h2 class="text-xl font-bold mt-6 mb-2">How you can delete your data</h2>
         <p>If you would like to delete your account, please contact <a class="text-blue-500 underline" href="mailto:jerome@smol.rsvp">jerome@smol.rsvp</a> and I'll get back to you ASAP.</p>
+
+        <h2 class="text-xl font-bold mt-6 mb-2">What we use your data for</h2>
+        <ul class="list-disc list-inside marker:text-blue-500">
+            <li>Listing your calendars and events</li>
+            <li>Inviting people to events on your calendar</li>
+        </ul>
     
         <h2 class="text-xl font-bold mt-6 mb-2">Contact Me</h2>
         <p>If you have any questions, reach out to me at <a class="text-blue-500 underline" href="mailto:jerome@smol.rsvp">jerome@smol.rsvp</a>.</p>
