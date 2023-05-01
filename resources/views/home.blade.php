1804956cb814a4241142bfd6631523ef2f2424c8
Jerome Paulos <jeromepaulos@yahoo.com>
2023-05-01 13:22:15 -0500

    Revert sign in button design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,20 @@
         <h1 class="sm:text-3xl text-2xl font-bold text-center">Free and easy RSVP pages<br><span class="text-blue-500">powered by Google Calendar</span></h1>
     
         <div class="text-center space-y-2">
-            {{-- <a href="{{ route('auth.redirect') }}" class="px-3 flex items-center gap-2 w-fit mx-auto border rounded-lg py-2 shadow-sm bg-blue-500 text-white border-blue-500 ">
+            <a href="{{ route('auth.redirect') }}" class="px-3 flex items-center gap-2 w-fit mx-auto border rounded-lg py-2 shadow-sm bg-blue-500 text-white border-blue-500 ">
                 <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" class="w-5">
                     <path fill="none" d="M0 0h24v24H0z"></path>
                     <path d="M3.064 7.51A9.996 9.996 0 0 1 12 2c2.695 0 4.959.99 6.69 2.605l-2.867 2.868C14.786 6.482 13.468 5.977 12 5.977c-2.605 0-4.81 1.76-5.595 4.123-.2.6-.314 1.24-.314 1.9 0 .66.114 1.3.314 1.9.786 2.364 2.99 4.123 5.595 4.123 1.345 0 2.49-.355 3.386-.955a4.6 4.6 0 0 0 1.996-3.018H12v-3.868h9.418c.118.654.182 1.336.182 2.045 0 3.046-1.09 5.61-2.982 7.35C16.964 21.105 14.7 22 12 22A9.996 9.996 0 0 1 2 12c0-1.614.386-3.14 1.064-4.49z"></path>
                 </svg>
     
                 <span>Login with Google Calendar</span>
-            </a> --}}
+            </a>
     
-            <a href="{{ route('auth.redirect') }}" class="w-52 mx-auto block">
+            {{-- <a href="{{ route('auth.redirect') }}" class="w-52 mx-auto block">
                 <img wdith="382" height="92" src="{{ asset('images/google-login.png') }}" alt="">
             </a>
     
-            <p class="text-sm text-gray-500">Coming soon, pending review from Google</p>
+            <p class="text-sm text-gray-500">Free while in beta</p> --}}
         </div>
     
         <div class="text-center">
