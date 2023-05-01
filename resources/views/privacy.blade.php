d6fc570f357c18bf3c82e1e11f9f3b931e6cf2ef
Jerome Paulos <jeromepaulos@yahoo.com>
2023-05-01 01:21:50 -0500

    Update privacy.blade.php

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -18,7 +18,7 @@
             <li>Inviting people to events on your calendar</li>
         </ul>
 
-        <p>smol.rsvp’s use and transfer of information received from Google APIs to any other app will adhere to <a href="https://developers.google.com/terms/api-services-user-data-policy">Google API Services User Data Policy</a>, including the Limited Use requirements.</p>
+        <p class="mt-2">smol.rsvp’s use and transfer of information received from Google APIs to any other app will adhere to <a class="text-blue-500 underline" href="https://developers.google.com/terms/api-services-user-data-policy">Google API Services User Data Policy</a>, including the Limited Use requirements.</p>
     
         <h2 class="text-xl font-bold mt-6 mb-2">Contact Me</h2>
         <p>If you have any questions, reach out to me at <a class="text-blue-500 underline" href="mailto:jerome@smol.rsvp">jerome@smol.rsvp</a>.</p>
